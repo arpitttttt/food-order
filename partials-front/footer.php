450f904343886e4d31fdc1562409cb1243f6f3c1
arpittttt <arpitsingh4488@gmail.com>
2024-03-12 22:59:45 +0530

    Food-Order Webapp

---
+++ partials-front/footer.php
@@ -0,0 +1,16 @@
+<!-- social Section Starts Here -->
+<section class="social">
+       
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            Designed By <a href="#">Anshika Singh</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
